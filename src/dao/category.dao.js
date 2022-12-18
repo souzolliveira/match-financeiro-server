2db06ba9309fdf287b12a28f238105d0992a1311
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2022-12-18 10:32:51 -0300

    fix: removing console.logs

--- src/dao/category.dao.js
+++ src/dao/category.dao.js
@@ -1,22 +1,12 @@
 const db = require("../config/database");
 
 exports.selectCategory = async ({ transaction_type, name, user_id }) => {
-  console.log(
-    0,
-    transaction_type,
-    user_id,
-    name,
-    `SELECT * FROM categories WHERE users_fk = $1 and transaction_type = $2 ${
-      name ? "and name = $3" : ""
-    }`
-  );
   const response = await db.query(
     `SELECT * FROM categories WHERE users_fk = $1 and transaction_type = $2 ${
       name ? "and name = $3" : ""
     }`,
     name ? [user_id, transaction_type, name] : [user_id, transaction_type]
   );
-  console.log(1, response);
   return response;
 };
 
